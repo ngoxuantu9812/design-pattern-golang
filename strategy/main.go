12d8a677955c129883bfdaaa425ba87235bcd32e
Tu Ngo <ngoxuantu9812@gmail.com>
2024-02-26 22:25:51 +0700

    Update strategy

--- strategy/main.go
+++ strategy/main.go
@@ -1,15 +1,22 @@
 package main
 
 func main() {
-	lfu := &lfu{}
+	lfu := &Lfu{}
 	cache := initCache(lfu)
+
 	cache.add("a", "1")
 	cache.add("b", "2")
+
 	cache.add("c", "3")
-	lru := &lru{}
+
+	lru := &Lru{}
 	cache.setEvictionAlgo(lru)
+
 	cache.add("d", "4")
-	fifo := &fifo{}
+
+	fifo := &Fifo{}
 	cache.setEvictionAlgo(fifo)
+
 	cache.add("e", "5")
+
 }
